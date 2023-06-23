7d11dc4d52232424e384644246340569d9d48817
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-23 09:35:52 -0300

    first button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div className="bg-red-50">page</div>;
+  return (
+    <div className="flex h-screen w-screen items-center bg-blue-950">
+      <div className="w-full text-center">
+        <button className="rounded-lg bg-white p-2 px-4">
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
