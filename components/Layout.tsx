013f602fed3c4932e5531d20d815087168cb6c44
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-28 21:38:44 -0300

    create layout

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import { Nav } from "@/components/Nav";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Layout({ children }: any) {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="flex h-screen w-screen items-center bg-blue-950">
+        <div className="w-full text-center">
+          <button
+            onClick={() => signIn("google")}
+            className="rounded-xl bg-white p-2 px-4"
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex min-h-screen bg-blue-950">
+      <Nav />
+      <div className="mb-2 mr-2 mt-2 flex-grow rounded-lg bg-white p-4">
+        {children}
+      </div>
+    </div>
+  );
+}
