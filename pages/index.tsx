15abcf591aaa129f93163166b8b4f0500f74e923
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-29 19:19:40 -0300

    first customizes dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
-  return <Layout>test</Layout>;
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="flex justify-between text-blue-900">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex gap-1 overflow-hidden rounded-lg bg-gray-300 text-black">
+          <Image
+            src={session?.user?.image as string}
+            alt="user image"
+            width={30}
+            height={30}
+          />
+          <span className="px-2 ">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
