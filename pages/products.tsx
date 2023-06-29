442e8b7d82cfc7703219335ba6e3f4dd6d8bed20
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-29 19:19:13 -0300

    create new product page

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,5 +1,15 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
-  return <Layout>Products page here</Layout>;
+  return (
+    <Layout>
+      <Link
+        className="rounded-md bg-blue-900 px-2 py-1 text-white"
+        href={"/products/new"}
+      >
+        Add new product
+      </Link>
+    </Layout>
+  );
 }
