80c8e597deaf1ad726b2c6ae6ca8a69dc707c792
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-28 21:39:07 -0300

    create layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,5 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className="flex h-screen w-screen items-center bg-blue-950">
-        <div className="w-full text-center">
-          <button className="rounded-xl bg-white p-2 px-4">
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex h-screen w-screen items-center bg-blue-950">
-      <div className="w-full text-center">
-        <button className="rounded-xl bg-white p-2 px-4">
-          Login with Google
-        </button>
-      </div>
-    </div>
-  );
+  return <Layout>test</Layout>;
 }
