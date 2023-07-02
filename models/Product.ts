b20eb2e2b4685e6a8f2d93ca8c1c7d40bba66d59
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-02 12:08:08 -0300

    refact product squema

--- models/Product.ts
+++ models/Product.ts
@@ -1,9 +1,15 @@
 import { Schema, model } from "mongoose";
 
-const ProductSquema = new Schema({
-  title: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-});
+const ProductSquema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 export const Product = model("product", ProductSquema);
