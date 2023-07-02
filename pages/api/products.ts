6cae121caf3c33bab50d8f0b67115383483c14f5
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-02 12:08:49 -0300

    conect and save new product into db

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,3 +1,24 @@
+import { mongooseConnect } from "@/lib/mongooseConnect";
+import { Product } from "@/models/Product";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handle(req: NextApiRequest, res: NextApiResponse) {}
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  await mongooseConnect();
+
+  if (method === "POST") {
+    const { title, description, price } = req.body;
+
+    const productDoc = await Product.create({
+      title,
+      description,
+      price,
+    });
+
+    res.json(productDoc);
+  }
+}
