357b0d817102bda32ae1f39257607203e1df143a
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-03 08:41:44 -0300

    rendering products

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,7 +1,21 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface IProducts {
+  title: string;
+  description?: string;
+  price: number;
+}
 
 export default function Products() {
+  const [products, setProducts] = useState<IProducts[]>([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((res) => setProducts(res.data));
+  }, []);
+
   return (
     <Layout>
       <Link
@@ -10,6 +24,24 @@ export default function Products() {
       >
         Add new product
       </Link>
+      <table className="basic">
+        <thead>
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => {
+            return (
+              <tr key={index}>
+                <td>{product.title}</td>
+                <td>buttons</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </Layout>
   );
 }
