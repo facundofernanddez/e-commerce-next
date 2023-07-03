623123235a5d07d5db5e1a272a5fbdba27ba86a3
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-03 07:31:25 -0300

    create interface for product model

--- models/Product.ts
+++ models/Product.ts
@@ -1,15 +1,21 @@
-import { Schema, model } from "mongoose";
+import { Document, Schema, model } from "mongoose";
 
-const ProductSquema = new Schema(
+const ProductSquema: Schema<IProduct> = new Schema(
   {
-    title: { type: String, required: true },
-    description: String,
-    price: { type: Number, required: true },
+    title: { type: String, required: true, trim: true },
+    description: { type: String, trim: true },
+    price: { type: Number, required: true, trim: true },
   },
   {
     versionKey: false,
     timestamps: true,
   }
 );
+
+interface IProduct extends Document {
+  title: string;
+  description?: string;
+  price: number;
+}
 
 export const Product = model("product", ProductSquema);
