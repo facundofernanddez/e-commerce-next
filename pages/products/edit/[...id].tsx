2546fb8cf631c0b9be4ff6bd3c65440b8c160965
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-04 09:32:34 -0300

    change for id name

--- pages/products/edit/[...id].tsx
+++ pages/products/edit/[...id].tsx
@@ -1,5 +1,8 @@
 import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
 
 export default function EditProductPage() {
+  const router = useRouter();
+
   return <Layout>Edit product here</Layout>;
 }
