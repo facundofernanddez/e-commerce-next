f9033dfdb8221732e5e7848e4f7003da40fc5bdd
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-05 09:47:41 -0300

    create new product form

---
+++ components/ProductForm.tsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
+import Layout from "./Layout";
+
+export default function ProductForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const router = useRouter();
+
+  const createProduct = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = { title, description, price };
+
+    await axios.post("/api/products", data);
+
+    router.push("/products");
+  };
+
+  return (
+    <Layout>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product name</label>
+        <input
+          type="text"
+          placeholder="product name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Description</label>
+        <textarea
+          placeholder="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label>Price in USD</label>
+        <input
+          type="number"
+          placeholder="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="btn-primary"
+        >
+          Save
+        </button>
+      </form>
+    </Layout>
+  );
+}
