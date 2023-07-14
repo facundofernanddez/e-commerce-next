e5d97077f38561dd3aa3566f163db967a44c7144
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-14 08:51:02 -0300

    add categories in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -91,6 +91,28 @@ export const Nav = () => {
           Products
         </Link>
         <Link
+          href={"/categories"}
+          className={
+            pathname.includes("categories") ? activeLink : inactiveLink
+          }
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
+            />
+          </svg>
+          Categories
+        </Link>
+        <Link
           href={"/settings"}
           className={pathname.includes("settings") ? activeLink : inactiveLink}
         >
