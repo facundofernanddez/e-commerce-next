c6df2ea9712ef3d44fadb30b2a5205bac5efb29c
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-15 10:00:01 -0300

    add category form

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -1,9 +1,28 @@
 import Layout from "@/components/Layout";
 
 export default function Categories() {
+  const saveCategory = () => {};
+
   return (
     <Layout>
       <h1>Categories</h1>
+      <label>New category name</label>
+      <form
+        onSubmit={saveCategory}
+        className="flex gap-1"
+      >
+        <input
+          className="mb-0"
+          type="text"
+          placeholder="Category name"
+        />
+        <button
+          type="submit"
+          className="btn-primary"
+        >
+          Save
+        </button>
+      </form>
     </Layout>
   );
 }
