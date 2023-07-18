0d5d4c5820c536a39daebb5b844e97376839ff35
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-18 10:11:04 -0300

    add parent category in the methods

--- pages/api/categories.ts
+++ pages/api/categories.ts
@@ -10,12 +10,12 @@ export default async function handle(
   await mongooseConnect();
 
   if (method === "GET") {
-    res.json(await Category.find());
+    res.json(await Category.find().populate("parent"));
   }
 
   if (method === "POST") {
-    const { name } = req.body;
-    const categoryDoc = await Category.create({ name });
+    const { name, parentCategory } = req.body;
+    const categoryDoc = await Category.create({ name, parent: parentCategory });
     res.json(categoryDoc);
   }
 }
