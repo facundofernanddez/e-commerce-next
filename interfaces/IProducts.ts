d3b7671aa3f40dceecf2369c26d25a88d5e58c50
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-22 10:26:32 -0300

    refact category property

--- interfaces/IProducts.ts
+++ interfaces/IProducts.ts
@@ -1,7 +1,10 @@
+import { ICategories } from "./ICategories";
+
 export interface IProducts {
   _id: string;
   title: string;
   description?: string;
   price: number;
   images?: any[];
+  category?: ICategories;
 }
