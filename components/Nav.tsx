5f062bf409da1b471292588ed74f8b4a933ea9a9
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-07-30 17:09:37 -0300

    separate logo in other component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Logo from "./Logo";
 
 export const Nav = ({ show }) => {
   const inactiveLink = "flex gap-1 p-1";
@@ -18,29 +19,10 @@ export const Nav = ({ show }) => {
     <aside
       className={
         (show ? "left-0" : "-left-full") +
-        " fixed top-0 h-full w-full bg-gray-100 p-4 text-gray-500"
+        " fixed top-0 h-full w-full bg-gray-100 p-4 text-gray-500 transition-all md:static md:w-auto"
       }
     >
-      <Link
-        className="mb-4 mr-4 flex gap-1"
-        href={"/"}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
-          />
-        </svg>
-        <span>ECommerceAdmin</span>
-      </Link>
+      <Logo />
       <nav className="flex flex-col gap-2">
         <Link
           href={"/"}
